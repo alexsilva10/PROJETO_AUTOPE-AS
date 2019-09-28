9c42fc2a983773f07d8ac454013c6fc681188bfc
ALEX DIAS <ALEX DIAS@ALEX>
2019-09-27 21:51:26 -0300

    Aprimoração dos pacotes(Controller e Conexao)

---
+++ src/Controller/Comando_Inicial.java
@@ -0,0 +1,12 @@
+
+package Controller;
+
+import View.TelaPrincipal;
+
+
+public class Comando_Inicial {
+    public static void main(String[] args) {
+        
+        TelaPrincipal principal = new TelaPrincipal();   
+    }
+}
