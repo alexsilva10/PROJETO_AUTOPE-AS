e68b4c6deed7c1d1215ab24bb6f4515e727a5bd9
User <User@DESKTOP-M6QL99S>
2019-10-01 10:45:28 -0300

    Tela principal e de cliente

--- src/View/Cliente_View.java
+++ src/View/Cliente_View.java
@@ -52,6 +52,8 @@ public class Cliente_View extends javax.swing.JInternalFrame {
         TxRG = new javax.swing.JTextField();
         jLabel21 = new javax.swing.JLabel();
         TxNome = new javax.swing.JTextField();
+        jLabel29 = new javax.swing.JLabel();
+        jComboBox2 = new javax.swing.JComboBox<>();
         jPJuridica = new javax.swing.JPanel();
         jLabel22 = new javax.swing.JLabel();
         TxIdjur = new javax.swing.JTextField();
@@ -65,6 +67,8 @@ public class Cliente_View extends javax.swing.JInternalFrame {
         jLabel26 = new javax.swing.JLabel();
         jLabel27 = new javax.swing.JLabel();
         TxNomeFan = new javax.swing.JTextField();
+        jLabel28 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
         jPanel1 = new javax.swing.JPanel();
         jRadioButton1 = new javax.swing.JRadioButton();
         jRadioButton2 = new javax.swing.JRadioButton();
@@ -75,6 +79,8 @@ public class Cliente_View extends javax.swing.JInternalFrame {
         FormattedTxtTelefone = new javax.swing.JFormattedTextField();
         jLabel18 = new javax.swing.JLabel();
         jTextFieldEmail = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         setBackground(new java.awt.Color(28, 203, 248));
         setClosable(true);
@@ -153,7 +159,7 @@ public class Cliente_View extends javax.swing.JInternalFrame {
         TxtComplemento.setBounds(330, 80, 240, 20);
 
         getContentPane().add(jPanel2);
-        jPanel2.setBounds(10, 310, 600, 120);
+        jPanel2.setBounds(10, 260, 590, 120);
 
         jPFisica.setBackground(new java.awt.Color(28, 203, 248));
         jPFisica.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Pessoa Física", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Sitka Small", 1, 11))); // NOI18N
@@ -164,51 +170,51 @@ public class Cliente_View extends javax.swing.JInternalFrame {
         jPFisica.add(jLabel2);
         jLabel2.setBounds(10, 20, 30, 20);
         jPFisica.add(TxtID);
-        TxtID.setBounds(30, 20, 30, 20);
+        TxtID.setBounds(50, 20, 40, 20);
 
         jLabel3.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
         jLabel3.setText("Matrícula:");
         jPFisica.add(jLabel3);
-        jLabel3.setBounds(60, 20, 60, 20);
+        jLabel3.setBounds(210, 20, 60, 20);
         jPFisica.add(TxtMatricula);
-        TxtMatricula.setBounds(120, 20, 70, 20);
+        TxtMatricula.setBounds(280, 20, 110, 20);
 
         jLabel4.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
         jLabel4.setText("Apelido:");
         jPFisica.add(jLabel4);
-        jLabel4.setBounds(430, 20, 50, 20);
+        jLabel4.setBounds(410, 50, 50, 20);
         jPFisica.add(TxtApelido);
-        TxtApelido.setBounds(480, 20, 100, 20);
+        TxtApelido.setBounds(480, 50, 100, 20);
 
         jLabel5.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
         jLabel5.setText("Estado Civil:");
         jPFisica.add(jLabel5);
-        jLabel5.setBounds(10, 50, 80, 20);
+        jLabel5.setBounds(10, 80, 80, 20);
 
         jComboBoxEstado.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
         jComboBoxEstado.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione", "Casado", "Solteiro" }));
         jPFisica.add(jComboBoxEstado);
-        jComboBoxEstado.setBounds(90, 50, 90, 20);
+        jComboBoxEstado.setBounds(90, 80, 90, 20);
 
         jLabel6.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
         jLabel6.setText("Dt de Nasc:");
         jPFisica.add(jLabel6);
-        jLabel6.setBounds(200, 80, 70, 20);
+        jLabel6.setBounds(200, 110, 70, 20);
 
         jLabel7.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
         jLabel7.setText("Estado Civil:");
         jPFisica.add(jLabel7);
-        jLabel7.setBounds(400, 80, 80, 20);
+        jLabel7.setBounds(400, 110, 80, 20);
 
         jLabel10.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
         jLabel10.setText("Sexo:");
         jPFisica.add(jLabel10);
-        jLabel10.setBounds(10, 80, 40, 20);
+        jLabel10.setBounds(10, 110, 40, 20);
 
         jComboBoxSexo.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
         jComboBoxSexo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione", "Masculino", "Feminino" }));
         jPFisica.add(jComboBoxSexo);
-        jComboBoxSexo.setBounds(50, 80, 130, 20);
+        jComboBoxSexo.setBounds(50, 110, 130, 20);
 
         try {
             TxDATA.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("##/##/####")));
@@ -216,7 +222,7 @@ public class Cliente_View extends javax.swing.JInternalFrame {
             ex.printStackTrace();
         }
         jPFisica.add(TxDATA);
-        TxDATA.setBounds(280, 80, 110, 20);
+        TxDATA.setBounds(280, 110, 110, 20);
 
         jComboBoxEstadoCivil.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
         jComboBoxEstadoCivil.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione", "Casado", "Solteiro" }));
@@ -226,28 +232,36 @@ public class Cliente_View extends javax.swing.JInternalFrame {
             }
         });
         jPFisica.add(jComboBoxEstadoCivil);
-        jComboBoxEstadoCivil.setBounds(480, 80, 100, 20);
+        jComboBoxEstadoCivil.setBounds(480, 110, 100, 20);
 
         jLabel19.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
         jLabel19.setText("CPF:");
         jPFisica.add(jLabel19);
-        jLabel19.setBounds(230, 50, 30, 14);
+        jLabel19.setBounds(230, 80, 30, 14);
 
         jLabel20.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
         jLabel20.setText("RG:");
         jPFisica.add(jLabel20);
-        jLabel20.setBounds(420, 50, 40, 14);
+        jLabel20.setBounds(420, 80, 40, 14);
         jPFisica.add(TxCPF);
-        TxCPF.setBounds(280, 50, 110, 20);
+        TxCPF.setBounds(280, 80, 110, 20);
         jPFisica.add(TxRG);
-        TxRG.setBounds(480, 50, 100, 20);
+        TxRG.setBounds(480, 80, 100, 20);
 
         jLabel21.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
         jLabel21.setText("Nome:");
         jPFisica.add(jLabel21);
-        jLabel21.setBounds(200, 20, 40, 14);
+        jLabel21.setBounds(10, 50, 40, 14);
         jPFisica.add(TxNome);
-        TxNome.setBounds(240, 20, 190, 20);
+        TxNome.setBounds(50, 50, 340, 20);
+
+        jLabel29.setText("Status:");
+        jPFisica.add(jLabel29);
+        jLabel29.setBounds(430, 20, 40, 14);
+
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Ativo", "Nao Ativo" }));
+        jPFisica.add(jComboBox2);
+        jComboBox2.setBounds(480, 20, 100, 20);
 
         jPJuridica.setBackground(new java.awt.Color(28, 203, 248));
         jPJuridica.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Pessoa Jurídica", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Sitka Small", 1, 11))); // NOI18N
@@ -268,26 +282,32 @@ public class Cliente_View extends javax.swing.JInternalFrame {
         jLabel23.setText("Rasão Social");
         jPJuridica.add(jLabel23, new org.netbeans.lib.awtextra.AbsoluteConstraints(121, 57, -1, -1));
         jPJuridica.add(TxRasao, new org.netbeans.lib.awtextra.AbsoluteConstraints(201, 54, 99, -1));
-        jPJuridica.add(TxCnpj, new org.netbeans.lib.awtextra.AbsoluteConstraints(35, 54, 74, -1));
+        jPJuridica.add(TxCnpj, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 50, 70, -1));
 
         jLabel24.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
-        jLabel24.setText("CNPJ");
-        jPJuridica.add(jLabel24, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 57, 30, -1));
+        jLabel24.setText("CNPJ:");
+        jPJuridica.add(jLabel24, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 57, 40, -1));
 
         jLabel25.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
-        jLabel25.setText("Matricula");
+        jLabel25.setText("Matricula:");
         jPJuridica.add(jLabel25, new org.netbeans.lib.awtextra.AbsoluteConstraints(132, 19, 60, -1));
         jPJuridica.add(TxMatriculaJuri, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 20, 99, -1));
-        jPJuridica.add(TxIncricao, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 54, 143, -1));
+        jPJuridica.add(TxIncricao, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 50, 143, -1));
 
         jLabel26.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
-        jLabel26.setText("Incrição Estadual");
-        jPJuridica.add(jLabel26, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 57, 100, -1));
+        jLabel26.setText("Incrição Estadual:");
+        jPJuridica.add(jLabel26, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 50, 100, -1));
 
         jLabel27.setFont(new java.awt.Font("Sitka Small", 0, 11)); // NOI18N
-        jLabel27.setText("Nome Fantazia");
-        jPJuridica.add(jLabel27, new org.netbeans.lib.awtextra.AbsoluteConstraints(312, 19, 90, -1));
-        jPJuridica.add(TxNomeFan, new org.netbeans.lib.awtextra.AbsoluteConstraints(409, 16, 144, -1));
+        jLabel27.setText("Nome Fantazia:");
+        jPJuridica.add(jLabel27, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 20, 90, -1));
+        jPJuridica.add(TxNomeFan, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 20, 144, -1));
+
+        jLabel28.setText("Status:");
+        jPJuridica.add(jLabel28, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 80, -1, 20));
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Ativo", "Nao Ativo" }));
+        jPJuridica.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 80, 140, -1));
 
         jLayeredPane1.setLayer(jPFisica, javax.swing.JLayeredPane.DEFAULT_LAYER);
         jLayeredPane1.setLayer(jPJuridica, javax.swing.JLayeredPane.DEFAULT_LAYER);
@@ -296,41 +316,37 @@ public class Cliente_View extends javax.swing.JInternalFrame {
         jLayeredPane1.setLayout(jLayeredPane1Layout);
         jLayeredPane1Layout.setHorizontalGroup(
             jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 610, Short.MAX_VALUE)
+            .addGroup(jLayeredPane1Layout.createSequentialGroup()
+                .addComponent(jPJuridica, javax.swing.GroupLayout.DEFAULT_SIZE, 590, Short.MAX_VALUE)
+                .addContainerGap())
             .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(jLayeredPane1Layout.createSequentialGroup()
                     .addComponent(jPFisica, javax.swing.GroupLayout.PREFERRED_SIZE, 590, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGap(0, 20, Short.MAX_VALUE)))
-            .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jLayeredPane1Layout.createSequentialGroup()
-                    .addComponent(jPJuridica, javax.swing.GroupLayout.PREFERRED_SIZE, 583, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGap(0, 27, Short.MAX_VALUE)))
+                    .addGap(0, 10, Short.MAX_VALUE)))
         );
         jLayeredPane1Layout.setVerticalGroup(
             jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 170, Short.MAX_VALUE)
+            .addGroup(jLayeredPane1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPJuridica, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(51, Short.MAX_VALUE))
             .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(jLayeredPane1Layout.createSequentialGroup()
                     .addContainerGap()
-                    .addComponent(jPFisica, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(44, Short.MAX_VALUE)))
-            .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jLayeredPane1Layout.createSequentialGroup()
-                    .addContainerGap(19, Short.MAX_VALUE)
-                    .addComponent(jPJuridica, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(48, Short.MAX_VALUE)))
+                    .addComponent(jPFisica, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
         );
 
         getContentPane().add(jLayeredPane1);
-        jLayeredPane1.setBounds(10, 130, 610, 170);
+        jLayeredPane1.setBounds(10, 100, 600, 160);
 
         jPanel1.setBackground(new java.awt.Color(28, 203, 248));
-        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Escolha o tipo do Cliente", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 14), new java.awt.Color(51, 51, 51))); // NOI18N
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Escolha o tipo do Cliente", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Sitka Small", 1, 14))); // NOI18N
         jPanel1.setForeground(new java.awt.Color(51, 51, 51));
 
         jRadioButton1.setBackground(new java.awt.Color(28, 203, 248));
         buttonGroup1.add(jRadioButton1);
-        jRadioButton1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jRadioButton1.setFont(new java.awt.Font("Sitka Small", 0, 14)); // NOI18N
         jRadioButton1.setText("Pessoa Fisica");
         jRadioButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -340,7 +356,7 @@ public class Cliente_View extends javax.swing.JInternalFrame {
 
         jRadioButton2.setBackground(new java.awt.Color(28, 203, 248));
         buttonGroup1.add(jRadioButton2);
-        jRadioButton2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jRadioButton2.setFont(new java.awt.Font("Sitka Small", 0, 14)); // NOI18N
         jRadioButton2.setText("Pessoa Juridica");
         jRadioButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -363,13 +379,13 @@ public class Cliente_View extends javax.swing.JInternalFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jRadioButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jRadioButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(jRadioButton1)
+                    .addComponent(jRadioButton2))
+                .addGap(0, 5, Short.MAX_VALUE))
         );
 
         getContentPane().add(jPanel1);
-        jPanel1.setBounds(40, 40, 530, 90);
+        jPanel1.setBounds(40, 40, 530, 60);
 
         jPanel3.setBackground(new java.awt.Color(28, 203, 248));
         jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Contato", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 1, 11))); // NOI18N
@@ -418,7 +434,7 @@ public class Cliente_View extends javax.swing.JInternalFrame {
                         .addComponent(jLabel18)
                         .addGap(18, 18, 18)
                         .addComponent(jTextFieldEmail)))
-                .addContainerGap(96, Short.MAX_VALUE))
+                .addContainerGap(86, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -436,9 +452,25 @@ public class Cliente_View extends javax.swing.JInternalFrame {
         );
 
         getContentPane().add(jPanel3);
-        jPanel3.setBounds(10, 440, 600, 80);
+        jPanel3.setBounds(10, 380, 590, 80);
 
-        setBounds(0, 0, 834, 593);
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        getContentPane().add(jScrollPane1);
+        jScrollPane1.setBounds(10, 460, 590, 100);
+
+        setBounds(0, 0, 798, 629);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jComboBoxEstadoCivilActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBoxEstadoCivilActionPerformed
@@ -492,6 +524,8 @@ public class Cliente_View extends javax.swing.JInternalFrame {
     private javax.swing.JTextField TxtNumero;
     private javax.swing.JTextField TxtRua;
     private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JComboBox<String> jComboBoxEstado;
     private javax.swing.JComboBox<String> jComboBoxEstadoCivil;
     private javax.swing.JComboBox<String> jComboBoxSexo;
@@ -515,6 +549,8 @@ public class Cliente_View extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel25;
     private javax.swing.JLabel jLabel26;
     private javax.swing.JLabel jLabel27;
+    private javax.swing.JLabel jLabel28;
+    private javax.swing.JLabel jLabel29;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
@@ -530,6 +566,8 @@ public class Cliente_View extends javax.swing.JInternalFrame {
     private javax.swing.JPanel jPanel3;
     private javax.swing.JRadioButton jRadioButton1;
     private javax.swing.JRadioButton jRadioButton2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextFieldEmail;
     // End of variables declaration//GEN-END:variables
 
